9436c3f688b9b53e1a28f420151b90ba5f82c963
Gulsum Gudukbay <gudukbay@gmail.com>
2017-12-04 21:30:32 +0300

    initial commit

---
+++ FaceDetection/src/FaceDetection.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : FaceDetection.c
+ Author      : Gulsum Gudukbay
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!");
+	return 0;
+}
