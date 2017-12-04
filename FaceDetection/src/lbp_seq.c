e369fd35a3526994f6e90d6832ba9f549a56487e
Gulsum Gudukbay <gudukbay@gmail.com>
2017-12-05 02:39:30 +0300

    testing

--- FaceDetection/src/lbp_seq.c
+++ FaceDetection/src/lbp_seq.c
@@ -15,7 +15,67 @@
 
 
 int main(void) {
-	puts("Hello World!");
+
+	int k = 5; //number of training images for each person
+	int n = 18; //number of people
+	int p = 20; //number of pictures for each person
+	int rows = 182;
+	int cols = 202;
+	int** closest_indices = alloc_2d_matrix(n, p-k);
+	int hist_size = 255;
+
+	//2d arrays of pictures for each picture for each people
+	int ****pictures = (int****)malloc(n * sizeof(int***));
+	int ***training_sets = (int***)malloc((k * n) * sizeof(int**));
+
+	for(int i = 0; i < n; i++)
+	{
+		pictures[i] = (int***)malloc(p * sizeof(int**));
+	}
+
+	char* filename;
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < p; j++)
+		{
+			filename = malloc(10*sizeof(char));
+			sprintf(filename, "%d.%d.txt", i, j);
+			pictures[i][j] = read_pgm_file(filename, rows, cols);
+			free(filename);
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < k; j++)
+		{
+			filename = malloc(10*sizeof(char));
+			sprintf(filename, "%d.%d.txt", i, j);
+			training_sets[k * i + j] = read_pgm_file(filename, rows, cols);
+			free(filename);
+		}
+	}
+
+	for(int i = 0; i < n; i++) //traverse each person
+	{
+		for(int j = k; j < p; j++) //traverse each test image for each person
+		{
+			int* test_img_hist;
+			create_histogram(test_img_hist, pictures[i][j], rows, cols);
+
+			//find closest person ids for each person, for each test image
+			closest_indices[i][j-k] = find_closest(training_sets, n, k, hist_size, test_img_hist);
+		}
+	}
+
+	for(int i = 0; i < n; i++) //traverse each person
+	{
+		for(int j = 0; j < n-p; j++) //traverse each test image for each person
+		{
+			printf("%d.%d.txt %d %d\n", i, j, closest_indices[i][j], i);
+		}
+	}
 	return 0;
 }
 
@@ -31,7 +91,6 @@ void create_histogram(int * hist, int ** img, int num_rows, int num_cols)
 		for(int j = 1; j < num_cols-1; j++)
 		{
 			cur_sum = 0;
-			index = 0;
 
 			if(img[i][j] < img[i-1][j-1])
 				cur_sum += 128;
@@ -72,24 +131,23 @@ int find_closest(int ***training_set, int num_persons, int num_training, int siz
 test_image)
 {
 	int** histograms = (int**)malloc(num_training * sizeof(int*));
-	for(int i = 0; i < num_training; i++)
-		create_histogram(histograms[i], training_set[i], 180, 200);
-	double* distances = (int*)malloc(num_training * sizeof(int));
 
-	double max_number = -999999;
-	int max_index = 0;
 	for(int i = 0; i < num_training; i++)
+		create_histogram(histograms[i], training_set[i], 182, 202);
+
+	double* distances = (double*)malloc(num_training * sizeof(double));
+
+	double min_number = 999999;
+	int min_index = 0;
+	for(int i = 0; i < num_training*num_persons; i++)
 	{
 		distances[i] = distance( histograms[i], test_image, size);
-		if(distances[i] > max_number)
+		if(distances[i] < min_number)
 		{
-			max_number = distances[i];
-			max_index = i;
+			min_number = distances[i];
+			min_index = i;
 		}
 	}
-	return max_index;
 
-
-
-
+	return min_index / num_persons;
 }
